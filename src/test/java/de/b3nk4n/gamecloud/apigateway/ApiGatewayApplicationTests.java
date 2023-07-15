6c341f61747abe7267247d55a678ebd643064d2b
Benjamin Kan <Kan@ibm.com>
2023-07-15 15:18:10 -0700

    Handle sessions with Spring Session Data Redis

--- src/test/java/de/b3nk4n/gamecloud/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/de/b3nk4n/gamecloud/apigateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,31 @@ package de.b3nk4n.gamecloud.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers // activates automatic startup and cleanup of test containers
 class ApiGatewayApplicationTests {
+    private static final int redisPort = 6379;
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.0.12"))
+            .withExposedPorts(redisPort);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", () -> redis.getHost());
+        registry.add("spring.redis.port", () -> redis.getMappedPort(redisPort));
+    }
 
     @Test
-    void contextLoads() {
+    void verifyThatSpringContextLoads() {
+        // no assert needed
     }
 
 }
