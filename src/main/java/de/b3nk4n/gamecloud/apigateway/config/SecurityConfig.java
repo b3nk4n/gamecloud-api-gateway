a830734a868b60d1be998118330d82922264c906
Benjamin Kan <Kan@ibm.com>
2023-07-31 15:06:15 -0700

    Add logout functionality
    via POST /logout, which forward the logout request to Keycloak

--- src/main/java/de/b3nk4n/gamecloud/apigateway/config/SecurityConfig.java
+++ src/main/java/de/b3nk4n/gamecloud/apigateway/config/SecurityConfig.java
@@ -4,16 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity,
+                                                  ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return httpSecurity
                 .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
                 //.formLogin(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults())
+                .logout(logoutSpec -> {
+                    final var oidcLogoutSuccessHandler =
+                            new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+                    oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUri}");
+                    logoutSpec.logoutSuccessHandler(oidcLogoutSuccessHandler);
+                })
                 .build();
     }
 }
