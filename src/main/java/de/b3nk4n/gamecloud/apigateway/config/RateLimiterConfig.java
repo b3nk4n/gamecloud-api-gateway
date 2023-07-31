080ab7ac11111bf7ae17abfd17d746679f8d3472
Benjamin Kan <Kan@ibm.com>
2023-07-31 01:10:20 -0700

    Do per-user rate limiting
    in case of authenticated user

--- src/main/java/de/b3nk4n/gamecloud/apigateway/config/RateLimiterConfig.java
+++ src/main/java/de/b3nk4n/gamecloud/apigateway/config/RateLimiterConfig.java
@@ -3,13 +3,15 @@ package de.b3nk4n.gamecloud.apigateway.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Configuration
 public class RateLimiterConfig {
     @Bean
     public KeyResolver keyResolver() {
-        // rate limiting grouped by a constant pseudo key as long as there is no user session/authentication concept
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
